d02e3fa92903c081755e4708db84e46bf571f3f1
LiorCohen0208 <liorcohen0208@gmail.com>
2025-01-26 16:12:12 +0200

    update

--- main.py
+++ main.py
@@ -54,4 +54,3 @@ def calc_mean_erp(trial_points_file, ecog_data_file):
     return fingers_erp_mean
 
 fingers_erp_mean = calc_mean_erp("events_file_ordered.csv", "brain_data_channel_one.csv")
-print("hi")
